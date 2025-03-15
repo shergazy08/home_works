4c87e19fffb749249a8d85da72d4a875e1cc6c07
shergazy08 <akylbekovsergazy14@gmail.com>
2025-03-15 17:59:32 +0600

    14.03 tapshyrma

---
+++ home_work.py
@@ -0,0 +1,26 @@
+def great():
+    print("pribet, polzabatel")
+great()
+great()
+
+
+def weather():
+    print("Сегодня 15°C, солнечно")
+weather()
+
+
+def favorite_food():
+    print("Мое любимое блюдо — пицца.")
+favorite_food()
+
+
+def sum_numbers():
+    print(f"7 + 3 = 10")
+sum_numbers()
+
+
+def repeat_phrase():
+    print("Учиться — это круто!")
+repeat_phrase()
+repeat_phrase()
+repeat_phrase()
