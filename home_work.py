21d25717aeb1cc66f0ca52d354bead58c8b2fd2f
shergazy08 <akylbekovsergazy14@gmail.com>
2025-03-20 19:39:00 +0600

    tush

---
+++ home_work.py
@@ -0,0 +1,17 @@
+import random
+
+a=random.randint(0,100)
+
+from random import randint
+
+s=randint(0,10)
+
+import datetime as dt
+
+nov=dt.datetime.now()
+
+from random import *
+
+randint(0,10)
+
+
